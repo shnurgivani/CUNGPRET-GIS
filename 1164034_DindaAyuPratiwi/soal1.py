0e4e0314346c4df56dcfc330b9451fa2cd92ae6b
Dinda Ayu Pratiwi <33660475+dindaayuprtw@users.noreply.github.com>
2018-10-19 06:04:52 -0700

    Update soal1.py

--- 1164034_DindaAyuPratiwi/soal1.py
+++ 1164034_DindaAyuPratiwi/soal1.py
@@ -1,10 +1,10 @@
-import shapefile
-w=shapefile.Writer()
-w.shapeType
-w.field("kolom1","C")
-w.field("kolom2","C")
-w.record("ngek","satu")
-w.record("ngok","dua")
-w.point(1,1)
-w.point(2,2)
-w.save("soal1")
+import shapefile #modul1
+w=shapefile.Writer() #deklarasi, dalam kurung boleh dikosongkan boleh tidak sesuai type
+w.shapeType #berfungsi menjalankan coding diatasnya
+w.field("kolom1","C") #nama kolom
+w.field("kolom2","C") #nama kolom
+w.record("ngek","satu") #isi dari kolom 1 ngek, dan kolom 2 satu
+w.record("ngok","dua") #isi dari kolom 1 ngok, dan kolom 2 dua
+w.point(1,1) #titik koordinat
+w.point(2,2) #titik koordinat
+w.save("soal1") #nama file
