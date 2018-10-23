99eb5ac0d2892555094e7be5365f1b02d514db80
Dinda Ayu Pratiwi <33660475+dindaayuprtw@users.noreply.github.com>
2018-10-23 06:10:27 -0700

    fifth

---
+++ 1164034_DindaAyuPratiwi/soal7.py
@@ -0,0 +1,13 @@
+import shapefile #modul1
+
+w=shapefile.Writer() #deklarasi, dalam kurung boleh dikosongkan boleh tidak sesuai type dan disini menggunakan shapeType 1
+w.shapeType #berfungsi menjalankan coding diatasnya
+
+w.field("kolom1","C") #nama kolom
+w.field("kolom2","C") #nama kolom
+
+w.record("ngek","satu") #isi dari kolom 1 ngek, dan kolom 2 satu
+
+w.poly(parts=[[[1,3],[5,3],[1,2],[5,2]]],shapeType=shapefile.POLYLINE) #titik koordinat
+
+w.save("soal7") #nama file
