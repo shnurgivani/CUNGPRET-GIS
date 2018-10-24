323ece6cc2a1618553674f03018163cf8225423e
Dinda Ayu Pratiwi <33660475+dindaayuprtw@users.noreply.github.com>
2018-10-24 05:29:38 -0700

    Update soal9.py

--- 1164034_DindaAyuPratiwi/soal9.py
+++ 1164034_DindaAyuPratiwi/soal9.py
@@ -7,6 +7,7 @@ w.field("kolom1","C") #nama kolom
 w.field("kolom2","C") #nama kolom
 
 w.record("ngek","satu") #isi dari kolom 1 ngek, dan kolom 2 satu
+w.record("crot","dua") #isi dari kolom 1 crot, dan kolom 2 dua
 
 w.poly(parts=[[[1,3],[5,3], [5,2],[1,2],[1,3]]],shapeType=shapefile.POLYLINE) #titik koordinat
 w.poly(parts=[[[1,6],[5,6], [5,9],[1,9],[1,6]]],shapeType=shapefile.POLYLINE) #titik koordinat
